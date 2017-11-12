Fixed Backpropogation Dimensions Fixed Loss Functions Fixed some data parsing

activation_functions.py
@@ -3,7 +3,7 @@ import numpy as np
 
 #NonLinear activation functions
 
-class Activation(object):
+class Activation_Function(object):
 	"""Abstract Class for activation function"""
 	def derivative(self):
 		"""
@@ -22,15 +22,15 @@ class Identity(object):
 	def derivative(self,z):
 		return np.ones(z.shape)
 
-class sigmoid(Activation):
+class Sigmoid(Activation_Function):
 
 	def __call__(self,z):
-		return float(1) / 1 + np.exp(-z)
+		return float(1) / (1 + np.exp(-z))
 
 	def derivative(self,z):
-		return (float(1) / 1 + np.exp(-z)) * (float(1) / 1 + np.exp(1 + z))
+		return self.__call__(z) * self.__call__(-z)
 
-class RLU(Activation):
+class RLU(Activation_Function):
 
 	def __call__(self, z):
 		return np.where(z <= 0,0,z)
@@ -38,7 +38,7 @@ class RLU(Activation):
 	def derivative(self, z):
 		return np.where(z <= 0,0,1)
 
-class Leaky_RLU(Activation):
+class Leaky_RLU(Activation_Function):
 
 	def __init__(self,alpha = 0.1):
 		self.alpha = alpha
@@ -49,7 +49,7 @@ class Leaky_RLU(Activation):
 	def derivative(self, z):
 		return np.where(x<=0,self.alpha,1)
 
-class softplus(Activation):
+class Softplus(Activation_Function):
 
 	def init(self):
 		pass
@@ -58,48 +58,13 @@ class softplus(Activation):
 		return np.log(1+np.exp(z))
 
 	def derivative(self,z):
-		return sigmoid(z)
+		return float(1) / 1 + np.exp(-z)
 
-# def sigmoid(x):
-# 	"""Sigmoid Activation"""
-# 	return 1 / (1 + math.pow(math.e,-x))
-#
-# def d_sigmoid(x):
-# 	"""Derivative of Sigmoid"""
-# 	return sigmoid(x) * sigmoid(1-x)
-#
-# def RLU(x):
-# 	"""Rectified Linear Unit (RLU) Activation"""
-# 	if x <= 0:
-# 		return 0
-# 	else:
-# 		return x
-#
-# def d_RLU(x):
-# 	"""Derivative of RLU"""
-# 	if x <= 0:
-# 		return 0
-# 	else:
-# 		return 1
-#
-# def leaky_RLU(x,alpha = 0.01):
-# 	"""Parameterized RLU Activation"""
-# 	if x <= 0:
-# 		alpha * x
-# 	else:
-# 		return x
-#
-# def d_leaky_RLU(x,alpha = 0.01):
-# 	"""Derivitave of Parameterized RLU"""
-# 	if x <= 0:
-# 		return alpha
-# 	else:
-# 		return 1
-#
-# def softplus(x):
-# 	"""Softmax activation"""
-# 	return math.log(1 + math.pow(math.e,x))
-#
-# def d_softplus(x):
-# 	"""Derivative of softmax activaiton """
-# 	return sigmoid(x)
+class Softmax(Activation_Function):
+    def __call__(self, x):
+        e_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
+        return e_x / np.sum(e_x, axis=-1, keepdims=True)
+
+    def derivative(self, x):
+        p = self.__call__(x)
+        return p * (1 - p)

deepnetwork.py
@@ -8,7 +8,7 @@ from terminaltables import AsciiTable
 import progressbar
 
 
-class deep_network(object):
+class deep_net(object):
 	"""
 	network archeticture is determined by structure of individual layers
 	"""
@@ -16,16 +16,18 @@ class deep_network(object):
 		self.layers = []
 		self.input_features = input_features
 		self.optimizer = optimizer
-		self.loss_function = loss_function
-		self.progressbar = progressbar.ProgressBar(widgets = bar_widgets)
+		self.loss_function = loss_function()
+		#self.progressbar = progressbar.ProgressBar(widgets = bar_widgets)
 
 	def add_layer(self,layer):
 		if self.layers:
 			layer.set_input_shape(input_shape = self.layers[-1].output_shape())
 		else:
 			layer.set_input_shape(input_shape = (self.input_features,))
+
 		if hasattr(layer,'initialize_weights'):
 			layer.initialize_weights(optimizer = self.optimizer)
+
 		self.layers.append(layer)
 
 	def intialize_network_layers(self,shape,layer_type,activation):
@@ -46,31 +48,39 @@ class deep_network(object):
 
 	def batch_train(self,X,y):
 		y_pred = self.forward_propogation(X)
-		loss = self.loss_function.loss(y,y_pred)
+		loss = np.mean(self.loss_function.loss(y,y_pred))
 		error = self.loss_function.gradient(y,y_pred)
-		self.backward_pass(error)
-		return loss
+		accuracy = self.loss_function.accuracy(y,y_pred)
+		#print 'error shape',error.shape
+		self.back_propogation(error)
+		return loss,accuracy
+
+	def batch_test(self,X,y):
+		y_pred = self.forward_propogation(X)
+		loss = np.mean(self.loss_function.loss(y, y_pred))
+		accuracy = self.loss_function.accuracy(y, y_pred)
+		return loss, accuracy
 
 	def fit(self,X,y,n_epochs,batch_size):
 		"""Trains using batch SGD for a number of epochs"""
 		#for _ in range(n_epochs):
-		for _ in self.progressbar(range(n_epochs)):
+		for _ in range(n_epochs):
 			batch_error = []
 			for X_batch,y_batch in batch_iterator(X,y):
-				loss = self.batch_train(X_batch,y_batch)
+				loss,accuracy = self.batch_train(X_batch,y_batch)
 				batch_error.append(loss)
 
 	def summary(self):
-		print AsciiTable([['Model Summary']]).table
 		print 'Data Input Shape %s' % str(self.layers[0].input_shape)
-		table_data = [["Layer Type","Number of Hidden Units","Number of Parameters","Output Shape",'Activation Function']]
+		print AsciiTable([['Model Summary']]).table
+		table_data = [["Layer Type","Output Shape"]]
 		total_params = 0
 		for layer in self.layers:
 			layer_name = layer.layer_name()
 			params = layer.parameters()
 			out_shape = layer.output_shape()
-			hidden_units = layer.n_neurons
-			table_data.append([layer_name,str(hidden_units),str(params),str(out_shape),str(layer.activation_function.name())])
+			#hidden_units = layer.n_neurons
+			table_data.append([layer_name,str(params),str(out_shape)])
 			total_params += params
 
 		print AsciiTable(table_data).table
@@ -79,7 +89,7 @@ class deep_network(object):
 
 
 if __name__ == '__main__':
-	net0 = deep_network(10,SGD,cross_entropy)
+	net0 = deep_net(10, SGD, Cross_Entropy)
 
 	net0.intialize_network_layers([5,3],Feedforward,RLU)
 

deepnetwork_layer.py
@@ -2,10 +2,10 @@ import numpy as np
 import math
 import copy
 from learning_optimizers import SGD
-from loss_functions import l2,cross_entropy
+from loss_functions import L2,Cross_Entropy
 
 class Layer(object):
-    """Abstract Class for Connected Layer"""
+    """Abstract Class for Layer"""
     def set_input_shape(self,input_shape):
         """
         Sets dimension of the inputs to this layer. Argument is a tuple (dim,)
@@ -45,61 +45,61 @@ class Feedforward(Layer):
     input_shape: tuple; Input shape of the layer.
     """
 
-    def __init__(self,n_neurons = 0,input_shape = None,activation_function = None):
+    def __init__(self,n_neurons = 0,input_shape = None):
         self.layer_input = None
         self.input_shape = input_shape
         self.n_neurons = n_neurons
         self.W = None
         self.w_0 = None
-        self.activation_function = activation_function()
-        self.trainable = True
+        #self.trainable = True
 
     def initialize_weights(self,optimizer):
         #Initialize Weights
         limit = 1 / math.sqrt(self.input_shape[0])
         self.W = np.random.uniform(-limit, limit, (self.input_shape[0], self.n_neurons))
         self.w0 = np.zeros((1, self.n_neurons))
-        self.W_optimizer = copy.copy(optimizer)
-        self.wo_optimizer = copy.copy(optimizer)
+        self.W_optimizer = optimizer()
+        self.w0_optimizer = optimizer()
 
     def parameters(self):
         #Returns the number of parameters
         return np.prod(self.W.shape) + np.prod(self.w0.shape)
 
-    def forward_pass(self,X):
+    def forward_pass(self,X,training = True):
         """
         Calculate the output of current layer and pass to next layer.
         Saves activation terms to be used in backpropogation
-        :param X: The output of the previous layer
-        :param A: The ACTIVATION of the previous layer (output before activation function is applied)
+        :param X: The output of the previous layer. Usually the output of the non-linear activation function.
         :return:
         """
         self.layer_input = X
-        self.activation_input = self.layer_input
-        self.activation_input = A
-        self.activation = np.dot(self.W,X) + self.w0
-        return self.activation_function(self.activation),self.activation_input
+        # print 'X Shape',X.shape
+        # print 'W Shape',self.W.shape
+        # print 'W0 Shape',self.w0.shape
+        self.activation = np.dot(X,self.W) + self.w0
+        return self.activation
 
     def backward_pass(self,previous_error):
         """
-        Update weights in layer by backpropogation and pass error term to next layer
-        :param acc_grad:
+        Update weights in layer and pass error term to next layer
+        :param previous_error:
         :return:
         """
         W = self.W
         w0 = self.w0
-
-        if self.trainable:
-            error_term = np.dot(W,previous_error) * self.activation_function.derivative(self.activation)
-            grad_w = np.mean(np.dot(self.activation_input,error_term),axis = 0)
-            grad_w0 = np.mean(error_term,axis = 0)
-
+        grad_w = np.dot(self.layer_input.T,previous_error)
+        grad_w0 = np.sum(previous_error,axis = 0,keepdims = True)
+        # print 'W shape',W.shape
+        #print 'Layer Input shape',self.layer_input.shape
+        #print 'previus_error.shape',previous_error.shape
+        #print 'grad_w shape',grad_w.shape
+        #print 'grad_w0 shape',grad_w0.shape
         # Update the layer weights
 
-        self.W = self.W_opt.update(self.W, grad_w)
-        self.w0 = self.w0_opt.update(self.w0, grad_w0)
+        self.W = self.W_optimizer.update(self.W, grad_w)
+        self.w0 = self.w0_optimizer.update(self.w0, grad_w0)
 
-        return error_term,W,w0
+        return np.dot(previous_error,W.T)
 
     def output_shape(self):
         return(self.n_neurons,)
@@ -108,12 +108,12 @@ class Activation(Layer):
     """
     Layer that applies an activation function e.g. sigmoid or RLU
     """
-    def __init__(self,activation_function):
+    def __init__(self,activation_function = None):
         self.activation_function = activation_function()
 
-    def forward_pass(self,X,training):
+    def forward_pass(self,X,training = True):
         self.layer_input = X
-        return self.activation_func(X)
+        return self.activation_function(X)
 
     def backward_pass(self,error):
         return error * self.activation_function.derivative(self.layer_input)

learning_optimizers.py
@@ -5,29 +5,14 @@ class SGD(object):
     Stochastic Gradient Descent (SGD)
     """
 
-    def __init__(self,learning_rate = 0.01,momentum = 0 ):
+    def __init__(self,learning_rate = 0.01,momentum = 0,batch_size = 1):
         self.learning_rate = learning_rate
         self.momentum = momentum
         self.w_updt = None
+        self.batch_size = batch_size
 
     def update(self,w,grad_w):
         if self.w_updt is None:
             self.w_updt = np.zeros(np.shape(w))
         self.w_updt = self.momentum * self.w_updt + (1-self.momentum) * grad_w
-        return w - self.learning_rate * self.w_updt
-
-class mini_batch_SGD(object):
-    """
-    Mini Batch SGD
-    """
-
-    def __init__(self,learning_rate = 0.01,momentum = 0 ):
-        self.learning_rate = learning_rate
-        self.momentum = momentum
-        self.w_updt = None
-
-    def update(self, w, grad_w):
-        if self.w_updt is None:
-            self.w_updt = np.zeros(np.shape(w))
-        self.w_updt = self.momentum * self.w_updt + (1 - self.momentum) * grad_w
-        return w - self.learning_rate * self.w_updt
+        return w - self.learning_rate * (1/self.batch_size) * self.w_updt

loss_functions.py
@@ -10,7 +10,7 @@ class Loss(object):
     def acc(selfself,y,y_pred):
         return 0
 
-class l2(Loss):
+class L2(Loss):
     def __init(self): pass
 
     def loss(self,y_true,y_pred):
@@ -19,15 +19,29 @@ class l2(Loss):
     def gradient(self,y_true,y_pred):
         return y_pred-y_true
 
-class cross_entropy(Loss):
+class Cross_Entropy(Loss):
     def __init(self): pass
 
     def loss(self,y_true,y_pred):
-        return y_true * np.log(y_pred) + (1-y_true) * np.log(1-y_pred)
+        # try:
+        y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
+        return - y_true * np.log(y_pred) - (1-y_true) * np.log(1-y_pred)
+        #except:
+            # print 'y_true.shape',y_true.shape
+            # print 'y_pred.shape',y_pred.shape
 
     def gradient(self,y_true,y_pred):
         return (y_true / y_pred) * (1-y_true) / (1-y_pred)
 
     def accuracy(self,y_true,y_pred):
-        total = np.shape(y_true)[0]
+        total = float(np.shape(y_true)[0])
+        # print 'y_pred shape',y_pred[0].shape
+        # print y_pred[0]
+        # print 'y_true shape',y_true[0].shape
+        # print y_true[0]
+        y_true = np.argmax(y_true,axis = 1)
+        y_pred = np.argmax(y_pred,axis = 1)
+        # print y_true[0]
+        # print y_pred[0]
         return np.sum(y_true == y_pred,axis = 0) / total
+

mnist_loader.py
@@ -29,7 +29,7 @@ def load_data():
     entry of the tuple.
     The ``validation_data`` and ``test_data`` are similar, except
     each contains only 10,000 images.
-    This is a nice dataR format, but for use in neural networks it's
+    This is a nice data format, but for use in neural networks it's
     helpful to modify the format of the ``training_data`` a little.
     That's done in the wrapper function ``load_data_wrapper()``, see
     below.
@@ -58,13 +58,19 @@ def load_data_wrapper():
     turn out to be the most convenient for use in our neural network
     code."""
     tr_d, va_d, te_d = load_data()
-    training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
+    training_inputs = [np.reshape(x, (784,)) for x in tr_d[0]]
     training_results = [vectorized_result(y) for y in tr_d[1]]
     training_data = zip(training_inputs, training_results)
-    validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
-    validation_data = zip(validation_inputs, va_d[1])
-    test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
-    test_data = zip(test_inputs, te_d[1])
+
+    validation_inputs = [np.reshape(x, (784,)) for x in va_d[0]]
+    #validation_data = zip(validation_inputs, va_d[1])
+    validation_results = [vectorized_result(y) for y in va_d[1]]
+    validation_data = zip(validation_inputs,validation_results)
+
+    test_inputs = [np.reshape(x, (784,)) for x in te_d[0]]
+    #test_data = zip(test_inputs, te_d[1])
+    test_results = [vectorized_result(y) for y in te_d[1]]
+    test_data = zip(test_inputs,test_results)
     return (training_data, validation_data, test_data)
 
 def vectorized_result(j):
@@ -72,7 +78,7 @@ def vectorized_result(j):
     position and zeroes elsewhere.  This is used to convert a digit
     (0...9) into a corresponding desired output from the neural
     network."""
-    e = np.zeros((10, 1))
+    e = np.zeros((10,))
     e[j] = 1.0
     return e
 

mnist_net.py
@@ -1,18 +1,46 @@
-from deepnetwork import deep_network
+from deepnetwork import deep_net
 from deepnetwork_layer import *
 from mnist_loader import load_data_wrapper
 from activation_functions import *
 from learning_optimizers import *
 from loss_functions import *
 from util import *
+from sklearn import datasets
+
+# data = datasets.load_digits()
+# X = data.data
+# y = data.target
+# X_test = X
+# y_test = y
+# print X.shape
+# print y.shape
+# #
+# def batch_iterator(X,y = None,batch_size = 64):
+# 	"""Generator function that divides data into batches for mini-batch SGD"""
+# 	points = X.shape[0]
+# 	for i in np.arange(0,points,batch_size):
+# 		begin,end = i,min(i+batch_size,points)
+# 		if y is not None:
+# 			yield X[begin:end],y[begin:end]
+# 		else:
+# 			yield X[begin:end]
+
 
 training_data,validation_data,test_data = load_data_wrapper()
 
-X_train,y_train = unwind_data(training_data)
-print len(X_train),len(y_train)
-mnist_net = deep_network(input_features =784 ,optimizer = SGD, loss_function = cross_entropy)
+X,y = unwind_data(training_data)
+X_test,y_test = unwind_data(test_data)
 
-mnist_net.add_layer(Feedforward(n_neurons = 10,activation_function = RLU))
-mnist_net.add_layer(Feedforward(n_neurons = 10,activation_function = sigmoid))
+print X[0].shape
+print y[0].shape
+n_features = 784
+mnist_net = deep_net(input_features = n_features, optimizer = SGD, loss_function = Cross_Entropy)
+mnist_net.add_layer(Feedforward(n_neurons = 10))
+mnist_net.add_layer(Activation(activation_function = RLU))
+mnist_net.add_layer(Feedforward(n_neurons = 10))
+mnist_net.add_layer(Activation(activation_function = Sigmoid))
 mnist_net.summary()
-mnist_net.fit(X_train,y_train,n_epochs = 1,batch_size = 64)
+mnist_net.fit(X,y,n_epochs = 1,batch_size = 256)
+
+loss,accuracy = mnist_net.batch_test(X_test,y_test)
+print accuracy